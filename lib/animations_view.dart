9d09a44eddb1556dd23ed5a416d3e5642c5d397e
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-22 01:50:14 +0300

    docs: add documation comments expalin how animated container work and how animation in animated foo widgets generally works

--- lib/animations_view.dart
+++ lib/animations_view.dart
@@ -21,6 +21,12 @@ class _AnimationsViewState extends State<AnimationsView> {
     /// any property
     /// onEnd is a method which invoked after animation ends
     /// you can change the default Curve.linear to any curve, to help choose see the docs at https://api.flutter.dev/flutter/animation/Curves-class.html
+    /// الفكر ة من الاخر عشان تعمل animation هي انك تغير القيم الي بتديها لل container وتعمل setState عشان تحدث الui  و تعمل build
+    /// فا الويدجت يعاد بنائها فا تلاقي تغير ف القيم الي كانت موجودة ف الويدجت القديمة فا تعمل الانيميشن
+    /// غالبا ده بيحصل عن طريق didUpdateWidget method in statful widget
+    /// سواء الكونتير هو الي حصل فوقيه مباشرة ال set state مع تغير القيم
+    /// او ويدجت فوق منه هي الي غيرت القيم وادتهاله  وعملت set state  هتشتغل عادي
+
     return Scaffold(
       body: Center(
         child: Column(
