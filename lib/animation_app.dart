e6c804561359d413840b52dfe45a0e7c3b9dd2d8
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-22 01:01:13 +0300

    extract MyApp to AnimationApp root widget

---
+++ lib/animation_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AnimationApp extends StatelessWidget {
+  const AnimationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
