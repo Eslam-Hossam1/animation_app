0a3587303bf89500bf3624b6090370582f18a250
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-22 03:11:59 +0300

    feat(animated-switcher-and-custom-loading-button):  create animated custom loading button and use  animated switcher to smothly change the button inner widget

--- lib/animations_view.dart
+++ lib/animations_view.dart
@@ -10,60 +10,57 @@ class AnimationsView extends StatefulWidget {
 }
 
 class _AnimationsViewState extends State<AnimationsView> {
-  bool isValuesChanged = false;
-  double scale = .5;
+  bool isLoading = false;
   @override
   Widget build(BuildContext context) {
-    /// Animated Widget
-    /// is a widget where you can animate the the Widget based on changable property variable during time
-    /// onEnd is a method which invoked after animation ends
-    /// you can change the default Curve.linear to any curve, to help choose see the docs at https://api.flutter.dev/flutter/animation/Curves-class.html
-    /// الفكر ة من الاخر عشان تعمل animation هي انك تغير القيم الي بتديها لل Animated Widget وتعمل setState عشان تحدث الui  و تعمل build
-    /// فا الويدجت يعاد بنائها فا تلاقي تغير ف القيم الي كانت موجودة ف الويدجت القديمة فا تعمل الانيميشن
-    /// غالبا ده بيحصل عن طريق didUpdateWidget method in statful widget
-    /// سواء الكونتير هو الي حصل فوقيه مباشرة ال set state مع تغير القيم
-    /// او ويدجت فوق منه هي الي غيرت القيم وادتهاله  وعملت set state  هتشتغل عادي
-
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ///بيتجاهل ال boundries بتاعت ابنه خلي بالك
-            ///اكنه بيتعامل زي ال stack كده
-            ///ده غير ال behavior بتاع ال container خلي بالك ف استعمل كل واحد ف المكان الي يناسبه
-            ///filterQuality لو عاوز تعمل blur او حاجة زي كده اثناء ال scalling
-            AnimatedScale(
-              // filterQuality: FilterQuality.low,
-              scale: scale,
-              onEnd: () {
-                log('animation end');
-              },
-              duration: Duration(
-                seconds: 1,
-              ),
-              child: Container(
-                width: 100,
-                height: 100,
-                color: Colors.red,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (isValuesChanged) {
-                  //initial values
-                  isValuesChanged = false;
-                  scale = .5;
-                } else {
-                  //new values
-                  isValuesChanged = true;
-                  scale = 2;
-                }
-                setState(() {});
-              },
-              child: Text('Start Animation'),
-            )
+            CustomAnimatedLoadingButton(),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomAnimatedLoadingButton extends StatefulWidget {
+  const CustomAnimatedLoadingButton({
+    super.key,
+  });
+
+  @override
+  State<CustomAnimatedLoadingButton> createState() =>
+      _CustomAnimatedLoadingButtonState();
+}
+
+class _CustomAnimatedLoadingButtonState
+    extends State<CustomAnimatedLoadingButton> {
+  bool isLoading = false;
+  double width = 300;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      width: width,
+      duration: Duration(milliseconds: 500),
+      child: ElevatedButton(
+        onPressed: () {
+          if (isLoading) {
+            width = 300;
+          } else {
+            width = 100;
+          }
+          isLoading = !isLoading;
+          setState(() {});
+        },
+        child: AnimatedSwitcher(
+          duration: Duration(seconds: 1),
+          child: isLoading
+              ? SizedBox.square(
+                  dimension: 25, child: CircularProgressIndicator())
+              : Text('Loading Button'),
         ),
       ),
     );
