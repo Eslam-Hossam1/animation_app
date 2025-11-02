77973d065e10f477692ac6be44b738389955558a
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-11-02 07:48:44 +0200

    feat(animated-list): create animated list

--- lib/animations_view.dart
+++ lib/animations_view.dart
@@ -17,7 +17,7 @@ class _AnimationsViewState extends State<AnimationsView> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              AnimatedCategories(),
+              CustomAnimatedList(),
             ],
           ),
         ),
@@ -26,56 +26,88 @@ class _AnimationsViewState extends State<AnimationsView> {
   }
 }
 
-class AnimatedCategories extends StatefulWidget {
-  const AnimatedCategories({
+class CustomAnimatedList extends StatefulWidget {
+  const CustomAnimatedList({
     super.key,
   });
 
   @override
-  State<AnimatedCategories> createState() => _AnimatedCategoriesState();
+  State<CustomAnimatedList> createState() => _CustomAnimatedListState();
 }
 
-enum SelectedWidget { first, second, third }
+class _CustomAnimatedListState extends State<CustomAnimatedList> {
+  List<String> items = [
+    'Eslam',
+    'Ahmed',
+    'Marwan',
+    'Abass',
+    'Salman',
+  ];
 
-class _AnimatedCategoriesState extends State<AnimatedCategories> {
-  SelectedWidget selectedWidget = SelectedWidget.first;
-  int counter = 0;
-
+  GlobalKey<AnimatedListState> listKey = GlobalKey<AnimatedListState>();
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        AnimatedSwitcher(
-          transitionBuilder: (child, animation) {
-            return ScaleTransition(
-              scale: animation,
-              child: child,
-            );
-          },
-          duration: Duration(milliseconds: 300),
-          child: switch (selectedWidget) {
-            SelectedWidget.first => FlutterLogo(size: 50),
-            SelectedWidget.second => Text('Second'),
-            SelectedWidget.third => Container(
-                width: 50,
-                height: 50,
-                color: Colors.red,
-              ),
-          },
-        ),
-        SizedBox(
-          height: 100,
-        ),
-        ElevatedButton(
-          onPressed: () {
-            setState(() {
-              selectedWidget = SelectedWidget
-                  .values[counter++ % SelectedWidget.values.length];
-            });
-          },
-          child: Text("Animate"),
-        ),
-      ],
+    return Expanded(
+      child: Column(
+        children: [
+          Expanded(
+            child: AnimatedList(
+              initialItemCount: items.length,
+              key: listKey,
+              itemBuilder: (context, index, animation) {
+                return ScaleTransition(
+                  scale: animation,
+                  child: GestureDetector(
+                    onTap: () {
+                      _removeItem(index);
+                    },
+                    child: Text(
+                      items[index],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          SizedBox(
+            height: 100,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              _addItem();
+            },
+            child: Text("Animate"),
+          ),
+          SizedBox(
+            height: 100,
+          ),
+        ],
+      ),
     );
+  }
+
+  void _removeItem(int index) {
+    listKey.currentState?.removeItem(
+      index,
+      (context, animation) => ScaleTransition(
+        scale: animation,
+        child: Container(
+          width: 200,
+          height: 100,
+          color: Colors.red,
+          child: Icon(
+            Icons.delete,
+            size: 20,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+    items.removeAt(index);
+  }
+
+  void _addItem() {
+    items.add('New Item ${items.length + 1}');
+    listKey.currentState?.insertItem(items.length - 1);
   }
 }
