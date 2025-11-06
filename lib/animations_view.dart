b53a4aef55c9d164a79d2fc2dd4459456a38adc1
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-11-06 09:11:11 +0200

    scale transition part 2 repeat,reverse,

--- lib/animations_view.dart
+++ lib/animations_view.dart
@@ -63,12 +63,65 @@ class _CustomFooTransitionState extends State<CustomFooTransition>
             size: 100,
           ),
         ),
-        const SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: () {
-            animationController.forward();
-          },
-          child: const Text('Toggle Animation'),
+        const SizedBox(height: 100),
+        Wrap(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                animationController.forward();
+              },
+              child: const Text('forward'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                animationController.reverse();
+              },
+              child: const Text('reverse'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                animationController.reset();
+              },
+              child: const Text('reset'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                animationController.repeat(reverse: true);
+              },
+              child: const Text('repeat reverse'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                animationController.repeat();
+              },
+              child: const Text('repeat without reverse'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                animationController.stop();
+              },
+              child: const Text('stop'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                scaleAnimation.addListener(
+                  customScaleAnimationListner,
+                );
+                animationController.addListener(
+                  customAnimationControllerListner,
+                );
+              },
+              child: const Text('add listeners'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                animationController
+                    .removeListener(customAnimationControllerListner);
+                animationController.removeListener(customScaleAnimationListner);
+              },
+              child: const Text('remove listeners'),
+            ),
+          ],
         ),
       ],
     );
